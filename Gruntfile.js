4d2c7095f6c316f2ac4b666491992918b1ea2bfe
Arugin <valera-sochi@bk.ru>
2015-03-27 16:16:50 +0300

    Autodeploy

---
+++ Gruntfile.js
@@ -0,0 +1,48 @@
+var pkg = require('./package.json');
+
+module.exports = function (grunt) {
+
+    grunt.initConfig({
+        shipit: {
+            options: {
+                // Project will be build in this directory.
+                workspace: '/var/tmp/workspace',
+
+                // Project will be deployed in this directory.
+                deployTo: '/home/ec2-user/node_hello_word',
+
+                // Repository url.
+                repositoryUrl: 'https://github.com/mayatskiy/node_hello_word',
+
+                // This files will not be transfered.
+                ignores: ['.git', 'node_modules'],
+
+                // Number of release to keep (for rollback).
+                keepReleases: 3
+            },
+
+            // Staging environment.
+            staging: {
+                servers: [process.env.SERVER_URL],
+                key: [process.env.PATH_TO_KEY]
+            }
+        }
+    });
+
+    /**
+     * Load shipit task.
+     */
+
+    grunt.loadNpmTasks('grunt-shipit');
+    grunt.loadNpmTasks('shipit-deploy');
+
+    grunt.registerTask('start', function () {
+        var done = this.async();
+        var current = grunt.config('shipit.options.deployTo') + '/current';
+        grunt.shipit.remote('cd ' + current + ' && npm start', done);
+    });
+
+    grunt.shipit.on('published', function () {
+        grunt.task.run(['start']);
+    });
+};
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Researching how to deploy on Amazon EC2' });
+  res.render('index', { title: 'Researching how to deploy on Amazon EC2. v2' });
 });
 
 module.exports = router;
